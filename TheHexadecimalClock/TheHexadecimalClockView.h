08c82e6a6873112fc3adffe5cccf7f38043ad8d1
Chloé Laisné <hello@chloelaisne.com>
2012-04-13 10:47:45 +0200

    Initial Commit

---
+++ TheHexadecimalClock/TheHexadecimalClockView.h
@@ -0,0 +1,13 @@
+//
+//  TheHexadecimalClockView.h
+//  TheHexadecimalClock
+//
+//  Created by Chloé Laisné on 4/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface TheHexadecimalClockView : ScreenSaverView
+
+@end
